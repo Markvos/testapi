1e5b50e44e540c2153e5982e7c83ed813fb0da94
Markvos <mark8vos@gmail.com>
2023-03-19 08:44:42 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const API_URL = 'https://zoeken.oba.nl/api/v1/search/?q=1984&authorization=ffbc1ededa6f23371bc40df1864843be&output=json';
+
+async function fetchData() {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data;
+}
+
+function displayResults(data) {
+  const resultsDiv = document.getElementById('results');
+  
+  data.results.map(result => {
+    const title = result.titles.short_title;
+    const coverImageURL = result.coverimages.coverimage[0].url;
+
+    const resultDiv = document.createElement('div');
+    resultDiv.innerHTML = `
+      <h2>${title}</h2>
+      <img src="${coverImageURL}" alt="${title}">
+    `;
+
+    resultsDiv.appendChild(resultDiv);
+  });
+}
+
+fetchData().then(data => displayResults(data));
